796d4e5a6bb29f0827f22e12c4a07bc40f1916ce
Surendar Singh <surendarsingh2002@gmail.com>
2023-02-11 18:06:59 +0530

    Admin and User html file added

--- app.js
+++ app.js
@@ -2,31 +2,41 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql2');
 const gTTS = require('gtts');
+const { randomBytes } = require('crypto');
 
 const app = express();
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'voice_assesment'
-});
+// const db = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'voice_assesment'
+// });
 
-db.connect((err) => {
-    if (err) throw err;
-    console.log('Connected to mysql database');
+// db.connect((err) => {
+//     if (err) throw err;
+//     console.log('Connected to mysql database');
+// });
+
+// Table 1 __ POST /admin
+// Primary Key, Admin Name, Text, UUID
+
+app.get('/admin', (req, res) => {
+    res.sendFile(__dirname + "/admin.html");
 });
 
 app.post('/admin', (req, res) => {
     let data = {
         name: req.body.name,
-        text: req.body.text
+        text: req.body.text,
+        text_id: randomBytes(10).toString('hex')
     };
     var gtts = new gTTS(text, 'en');
     
-    gtts.save('$uuidno.mp3', function (err, result){
+    gtts.save(`${text_id}.mp3`, function (err, result){
         if(err) { throw new Error(err); }
-        console.log('$uuid audio file created');
+        console.log(`${text_id} audio file created`);
     });
 
     let sql = 'INSERT INTO users SET ?';
@@ -38,22 +48,14 @@ app.post('/admin', (req, res) => {
 
 });
 
-app.get('/', (req, res) => {
-    res.send(`
-        <form action="/input" method="post">
-        <input type="text" name="userInput">
-        <input type="submit" value="Submit">
-        </form>
-    `);
+// Table 2 __ POST /input
+// Primary Key, User Name, Text uploaded by the user, uuid, Accurary
+
+app.get('/user', (req, res) => {
+    res.sendFile(__dirname + "/user.html");
 });
 
-// Table 1 __ POST /admin
-// Primary Key, Admin Name, Text, UUID
-
-// Table 2 __ POST /input
-// Primary Key, User Name, Text uploaded by the user, Accurary
-
-app.post('/input', (req, res) => {
+app.post('/user', (req, res) => {
     let data = { userInput: req.body.userInput };
 
     let sql = 'INSERT INTO inputs SET ?';
