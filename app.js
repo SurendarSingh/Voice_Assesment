754d7a57a083cbbdd814c5c5f48b0d2889b06809
Surendar Singh <surendarsingh2002@gmail.com>
2023-02-14 17:11:58 +0530

    html to ejs files

--- app.js
+++ app.js
@@ -4,44 +4,46 @@ const mysql = require('mysql2');
 const gTTS = require('gtts');
 const stringSimilarity = require("string-similarity");
 const { randomBytes } = require('crypto');
+const fs = require('fs');
 
 const app = express();
+
+app.set("view engine", "ejs");
+
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'xyz'
-});
+// const db = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'xyz'
+// });
 
-db.connect((err) => {
-    if (err) throw err;
-    console.log('Connected to mysql database');
-});
+// db.connect((err) => {
+//     if (err) throw err;
+//     console.log('Connected to mysql database');
+// });
 
 // Table: admin_task_1
 // text_id, admin_name, text
 
 app.get('/admin', (req, res) => {
-
-
-    res.sendFile(__dirname + "/admin.html");
+    res.render("admin");
 });
 
 app.post('/admin', (req, res) => {
     let data = {
-        name: req.body.name,
         text: req.body.text,
         text_id: randomBytes(10).toString('hex')
     };
     
-    var gtts = new gTTS('Hi bro how are you', 'en');
+    var gtts = new gTTS(text, 'en');
     gtts.save(`${__dirname}/Task_2_AudioFiles/${text_id}.mp3`, function (err, result){
         if(err) { throw new Error(err); }
         console.log(`${text_id} audio file created`);
     });
 
+    // text, text_id will be stored in admin_task_1
     let sql = 'INSERT INTO users SET ?';
     let query = connection.query(sql, data, (err, result) => {
         if (err) throw err;
@@ -56,16 +58,27 @@ app.post('/admin', (req, res) => {
 
 app.get('/user', (req, res) => {
 
-    // Radomly select one audio file from Task_2_AudioFiles Folder
-
-    res.sendFile(__dirname + "/user.html");
+    var audioPath = __dirname + "/public/Task_2_AudioFiles/";
+    var files = fs.readdirSync(audioPath);
+    let chosenFile = audioPath + files[Math.floor(Math.random() * files.length)];
+    
+    res.render("user", {audioFile: chosenFile});
 });
 
 app.post('/user', (req, res) => {
-    let data = { userInput: req.body.userInput };
+    let data = { 
+        userName: req.body.userName,
+        userText: req.body.userText,
+        text_id: req.body.text_id
+    };
 
-    var accuracy = stringSimilarity.compareTwoStrings("healed", "sealed") * 100;
+    // Code to retrieve text through text_id from admin_task_1 Database
+    var text = "";
 
+
+    var accuracy = Math.round(stringSimilarity.compareTwoStrings(userText, text) * 100);
+
+    // text_id, user_name, user_text, accuracy store in user_task_1
     let sql = 'INSERT INTO inputs SET ?';
     let query = connection.query(sql, data, (err, result) => {
         if (err) throw err;
@@ -73,8 +86,6 @@ app.post('/user', (req, res) => {
         res.send('Input stored in database');
     });
 });
-
-console.log(Math.round(stringSimilarity.compareTwoStrings("I went to john house to buy bread", "to buy bread I went john house") * 100));
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
