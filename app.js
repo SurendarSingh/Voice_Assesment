79b6f2345c7c98ff6155b9019c4a0bb4ea397265
Surendar Singh <surendarsingh2002@gmail.com>
2023-02-26 23:00:33 +0530

    code updated

--- app.js
+++ app.js
@@ -13,16 +13,16 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-// const db = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     database: 'xyz'
-// });
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'vatask2_db'
+});
 
-// db.connect((err) => {
-//     if (err) throw err;
-//     console.log('Connected to mysql database');
-// });
+db.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to mysql database');
+});
 
 // Table: admin_task_1
 // text_id, admin_name, text
@@ -32,24 +32,22 @@ app.get('/admin', (req, res) => {
 });
 
 app.post('/admin', (req, res) => {
-    let data = {
-        text: req.body.text,
-        text_id: randomBytes(10).toString('hex')
-    };
+    let text = req.body.text;
+    let text_id = randomBytes(10).toString('hex');
     
     var gtts = new gTTS(text, 'en');
-    gtts.save(`${__dirname}/Task_2_AudioFiles/${text_id}.mp3`, function (err, result){
+    gtts.save(`${__dirname}/public/Task_2_AudioFiles/${text_id}.mp3`, function (err, result){
         if(err) { throw new Error(err); }
         console.log(`${text_id} audio file created`);
     });
 
     // text, text_id will be stored in admin_task_1
-    let sql = 'INSERT INTO users SET ?';
-    let query = connection.query(sql, data, (err, result) => {
+    var sql = `INSERT INTO admin_task_1 (text, text_id) VALUES ("${text}", "${text_id}")`;
+    db.query(sql, function (err, result) {
         if (err) throw err;
-        console.log(result);
-        res.send('Data inserted successfully');
     });
+
+    res.send('Data inserted successfully');
 
 });
 
@@ -58,7 +56,7 @@ app.post('/admin', (req, res) => {
 
 app.get('/user', (req, res) => {
 
-    var audioPath = __dirname + "/public/Task_2_AudioFiles/";
+    var audioPath = "Task_2_AudioFiles/";
     var files = fs.readdirSync(audioPath);
     let chosenFile = audioPath + files[Math.floor(Math.random() * files.length)];
     
@@ -72,19 +70,24 @@ app.post('/user', (req, res) => {
         text_id: req.body.text_id
     };
 
+
     // Code to retrieve text through text_id from admin_task_1 Database
     var text = "";
+    console.log("done");
 
 
-    var accuracy = Math.round(stringSimilarity.compareTwoStrings(userText, text) * 100);
+    var accuracy = 100;
+    res.send('Your Accuracy is ' + accuracy + "%");
+
+    Math.round(stringSimilarity.compareTwoStrings(userText, text) * 100);
 
     // text_id, user_name, user_text, accuracy store in user_task_1
-    let sql = 'INSERT INTO inputs SET ?';
-    let query = connection.query(sql, data, (err, result) => {
-        if (err) throw err;
-        console.log(result);
-        res.send('Input stored in database');
-    });
+    // let sql = 'INSERT INTO inputs SET ?';
+    // let query = connection.query(sql, data, (err, result) => {
+    //     if (err) throw err;
+    //     console.log(result);
+    //     res.send('Your Accuracy is ' + accuracy + "%");
+    // });
 });
 
 app.listen(3000, () => {
